ad9d571d761c1c8546ac054eb60becefc8af1cd6
nguyennhatthuan3011 <50845191+nguyennhatthuan3011@users.noreply.github.com>
2019-07-10 17:29:07 +0700

    validate form, check same username and same email

--- render.js
+++ render.js
@@ -1,19 +1,54 @@
 var renderListPost = function(data, element) {
     "use strict";
+
     var template = "";
 
+    // console.log(headIdUser.id);
 
     if (data.length === 0) {
-        template = "";
+        template += '<tr>';
+        template += '<td colspan="7">' + ' Not Found User ' + '</td>';
+        template += '</tr>'
+    } else {
+        var headUser = _.head(data);
+        var headIdUser = headUser.id;
+        headUserId = headIdUser;
+    }
+    // var a = _.filter(data, ['username', 'Kamren']);
+    // console.log(a);
+    _.forEach(data, function(value) {
+        template += '<tr>';
+        template += '<td>' + value.id + '</td>';
+        template += '<td>' + value.name + '</td>';
+        template += '<td>' + value.username + '</td>';
+        template += '<td>' + value.email + '</td>';
+        template += '<td>' + value.phone + '</td>';
+        template += '<td>' + value.website + '</td>';
+        template += '<td class="colEdit"><button class="btn btn-warning" onclick="editPost(' + value.id + ')" data-toggle="modal" data-target="#edituserModal">Edit</button></td>';
+        template += `<td class="colDel"><button class="btn btn-danger" onclick="deletePost('${value.id}')">Delete</button></td>`;
+        template += '<td class="colEdit"><button class="btn btn-success" onclick="getTodos(' + value.id + ')">Detail</button></td>';
+        template += '</tr>';
+    });
+    element.innerHTML = template;
+}
+
+var renderListTodos = function(data, element) {
+    "use strict"
+    var template = "";
+    if (data.length === 0) {
+        template += '<tr>';
+        template += '<td colspan="4">' + ' Not Found Data ' + '</td>';
+        template += '</tr>'
     }
 
-
-    _.forEach(data, function(value, key) {
-        console.log();
-
+    _.forEach(data, function(value) {
+        template += '<tr>';
+        template += '<td>' + value.title + '</td>';
+        template += '<td>' + value.completed + '</td>';
+        template += '<td class="colEdit"><button class="btn btn-warning" onclick="editTodos(' + value.id + ')" data-toggle="modal" data-target="#edittodoModal">Edit</button></td>';
+        template += `<td class="colDel"><button class="btn btn-danger" onclick="deleteTodos('${value.id}')">Delete</button></td>`;
+        template += '</tr>';
     });
-};
-
-var renderListTodos = function(data) {
+    element.innerHTML = template;
 
 };
--- scripts.js
+++ scripts.js
@@ -4,22 +4,20 @@ document.onreadystatechange = function() {
     }
 };
 
+var defaultUserId = 1;
+var headUserId = 0;
+var todosId = '';
 
 function initApplication() {
-    const defaultUserId = 1;
+    // const defaultUserId = 1;
     showListPosts();
     getTodos(defaultUserId);
 }
 
 
-
-var usrId = 1;
-
-var todosId = '';
-
 // (function() {
 //     showListPosts();
-//     getTodos(usrId);
+//     getTodos(defaultUserId);
 
 // })();
 
@@ -31,53 +29,62 @@ function showListPosts() {
 
     return callApi('users', 'GET')
         .then(function(response) {
-            if (response.length > 0) {
-                let template = '';
-                for (i = 0; i < response.length; i++) {
-                    template += '<tr>';
-                    template += '<td>' + response[i].id + '</td>';
-                    template += '<td>' + response[i].name + '</td>';
-                    template += '<td>' + response[i].username + '</td>';
-                    template += '<td>' + response[i].email + '</td>';
-                    template += '<td>' + response[i].phone + '</td>';
-                    template += '<td>' + response[i].website + '</td>';
-                    template += '<td class="colEdit"><button class="btn btn-warning" onclick="editPost(' + response[i].id + ')" data-toggle="modal" data-target="#edituserModal">Edit</button></td>';
-                    template += `<td class="colDel"><button class="btn btn-danger" onclick="deletePost('${response[i].id}')">Delete</button></td>`;
-                    template += '<td class="colEdit"><button class="btn btn-success" onclick="getTodos(' + response[i].id + ')">Detail</button></td>';
-                    template += '</tr>';
-                }
-                // console.log($el)
+            renderListPost(response, $el);
+            // defaultUserId = headIdUser;
+        });
+};
 
-                $el.innerHTML = template;
-            } else {
-                $el.innerHTML = `
-                <tr>
-                    <td colspan="7"> Data not found </td>
-                </tr>
-                `;
-            }
-        })
+
+// CREATE USER
+addPost = function() {
+
+    var fName = document.forms["createUserForm"]["name"].value;
+    var fUsername = document.forms["createUserForm"]["username"].value;
+    var fEmail = document.forms["createUserForm"]["email"].value;
+    var fPhone = document.forms["createUserForm"]["phone"].value;
+    var fWebsite = document.forms["createUserForm"]["website"].value;
+    if (fName == "" || fUsername == "" || fEmail == "" || fPhone == "" || fWebsite == "") {
+        alert("All Field Must Be Filled!")
+        return false;
+    } else {
+        var name = document.getElementById("name").value;
+        var username = document.getElementById("username").value;
+        var email = document.getElementById("email").value;
+        var phone = document.getElementById("phone").value;
+        var website = document.getElementById("website").value;
+
+        callApi('users', 'GET')
+            .then(function(response) {
+                var filterUsernam = _.filter(response, ['username', username]);
+                var filterEmail = _.filter(response, ['email', editEmail]);
+                if (filterUsernam.length === 0) {
+                    var data = { "name": name, "username": username, "email": email, "phone": phone, "website": website }
+                    callApi('users', 'POST', data)
+                        .then(function(response) {
+                            document.getElementById("createUserForm").reset();
+                        })
+                        .then(function(_) {
+                            alert("Add User Success");
+                        })
+                        .then(function(_) {
+                            return showListPosts();
+                        })
+                    $('#createuserModal').modal('hide')
+                } else {
+                    alert("Username or Email exists!")
+                    return false;
+                }
+            })
+    }
 }
 
 
-// POST USERS
-addPost = function() {
-    var name = document.getElementById("name").value;
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    var website = document.getElementById("website").value;
-    var data = { "name": name, "username": username, "email": email, "phone": phone, "website": website }
 
-    callApi('users', 'POST', data)
-        // .then(response => console.log('Success: ', JSON.stringify(response)))
-        // .catch(error => console.error('Error:', error));
-        .then(function(response) {
 
-            document.getElementById("createUserForm").reset();
-        })
-    return showListPosts();
+// CANCEL CREATE USERS
 
+cancelAdd = function() {
+    document.getElementById("createUserForm").reset();
 }
 
 //EDIT USERS
@@ -85,7 +92,7 @@ editPost = function(id) {
     // Code of function edit post
     callApi('users/' + id, 'GET')
         .then(function(response) {
-            document.getElementById("editId").value = response.id;
+            defaultUserId = response.id;
             document.getElementById("editName").value = response.name;
             document.getElementById("editUsername").value = response.username;
             document.getElementById("editEmail").value = response.email;
@@ -97,61 +104,70 @@ editPost = function(id) {
 // SAVE EDIT POST
 
 savePost = function() {
-    var editName = document.getElementById("editName").value;
-    var editUsername = document.getElementById("editUsername").value;
-    var editEmail = document.getElementById("editEmail").value;
-    var editPhone = document.getElementById("editPhone").value;
-    var editWebsite = document.getElementById("editWebsite").value;
-    var data = { "name": editName, "username": editUsername, "email": editEmail, "phone": editPhone, "website": editWebsite };
+
+    var fEditName = document.forms["editUserForm"]["editName"].value;
+    var fEditUsername = document.forms["editUserForm"]["editUsername"].value;
+    var fEditEmail = document.forms["editUserForm"]["editEmail"].value;
+    var fEditPhone = document.forms["editUserForm"]["editPhone"].value;
+    var fEditWebsite = document.forms["editUserForm"]["editWebsite"].value;
+    if (fEditName == "" || fEditUsername == "" || fEditEmail == "" || fEditPhone == "" || fEditWebsite == "") {
+        alert("All Field Must Be Filled!")
+        return false;
+    } else {
+        var editName = document.getElementById("editName").value;
+        var editUsername = document.getElementById("editUsername").value;
+        var editEmail = document.getElementById("editEmail").value;
+        var editPhone = document.getElementById("editPhone").value;
+        var editWebsite = document.getElementById("editWebsite").value;
 
 
+        // FILTER USERNAME AND EMAIL
 
-    callApi('users/' + id, 'PUT', data)
-        .then(function(_) {
-            showListPosts();
-            alert("Edit Success");
-        });
+
+        callApi('users', 'GET')
+            .then(function(response) {
+                var filterUsernam = _.filter(response, ['username', editUsername]);
+                var filterEmail = _.filter(response, ['email', editEmail]);
+                if (filterUsernam.length === 0) {
+                    var data = { "name": editName, "username": editUsername, "email": editEmail, "phone": editPhone, "website": editWebsite };
+
+                    callApi('users/' + defaultUserId, 'PUT', data)
+                        .then(function(_) {
+                            showListPosts();
+                            alert("Edit Success");
+                        });
+                    $('#edituserModal').modal('hide')
+                } else {
+                    alert("Username or Email exists!")
+                    return false;
+                }
+            })
+    }
 };
-
 
 //DELETE USERS
 deletePost = function(id) {
         // Code of function delete post
         callApi('users/' + id, 'DELETE')
-            .then(function() {
-                showListPosts();
+            .then(function(response) {
+                console.log(response);
+                return showListPosts();
+                // getTodos(headUserId);
+            })
+            .then(() => {
+                getTodos(headUserId);
             });
     }
     // GET TODOS
 
 function getTodos(id) {
-    usrId = id
+    defaultUserId = id
+    const $el = document.getElementById("todos");
     callApi('todos?userId=' + id, 'GET')
         .then(function(response) {
-            if (response.length > 0) {
-                let template = '';
-                for (i = 0; i < response.length; i++) {
-                    template += '<tr>';
-                    template += '<td>' + response[i].title + '</td>';
-                    template += '<td>' + response[i].completed + '</td>';
-                    template += '<td class="colEdit"><button class="btn btn-warning" onclick="editTodos(' + response[i].id + ')" data-toggle="modal" data-target="#edittodoModal">Edit</button></td>';
-                    template += `<td class="colDel"><button class="btn btn-danger" onclick="deleteTodos('${response[i].id}')">Delete</button></td>`;
-                    template += '</tr>';
-                }
-
-                let $el = document.getElementById("todos");
-                $el.innerHTML = template;
-
-            } else {
-                let template = '';
-                template += '<tr>';
-                template += '<td colspan="4">' + 'User has no information' + '</td>';
-                template += '<tr>';
-
-                let $el = document.getElementById("todos");
-                $el.innerHTML = template;
-            }
-        })
+            renderListTodos(response, $el)
+        });
+    document.getElementById("titleTodosList").innerHTML = defaultUserId;
 }
 
 
@@ -159,26 +175,55 @@ function getTodos(id) {
 
 createTodos = function() {
 
-    var userId = document.getElementById("userId").value;
-    var title = document.getElementById("title").value;
-    var radio = document.getElementsByName("completed");
-    for (i = 0; i < radio.length; i++) {
-        if (radio[i].checked) {
-            var completed = radio[i].value
+    var fUserID = document.forms["createTodosForm"]["userId"].value;
+    var fTitle = document.forms["createTodosForm"]["title"].value;
+    var fComplete = document.forms["createTodosForm"]["completed"].value;
+    if (fUserID == "" || fTitle == "" || fComplete == "") {
+        alert("All Field Must Be Filled!")
+        return false;
+    } else {
+        var userId = document.getElementById("userId").value;
+        var title = document.getElementById("title").value;
+        var radio = document.getElementsByName("completed");
+        for (i = 0; i < radio.length; i++) {
+            if (radio[i].checked) {
+                var completed = radio[i].value
+            }
         }
-    }
-    var data = {
-        "userId": userId,
-        "title": title,
-        "completed": completed
-    }
 
-    callApi('todos', 'POST', data)
-        .then(function(response) {
-            getTodos(userId);
-            alert("Success")
-            document.getElementById("createTodosForm").reset();
-        })
+        // CODE FILTER TODOS
+
+        callApi('todos?userId=' + userId, 'GET')
+            .then(function(response) {
+                var filterTitle = _.filter(response, ['title', title]);
+                if (filterTitle.length === 0) {
+                    var data = {
+                        "userId": userId,
+                        "title": title,
+                        "completed": completed
+                    }
+
+                    callApi('todos', 'POST', data)
+                        .then(function(response) {
+                            getTodos(userId);
+                            document.getElementById("createTodosForm").reset();
+                        })
+                        .then(function() {
+                            alert("Create Todos Success");
+                        })
+                    $('#createtodoModal').modal('hide')
+                } else {
+                    alert("Title exists");
+                    return false;
+                }
+            })
+    }
+}
+
+// CANCEL CREATE TODOS
+
+cancelAddTodos = function() {
+    document.getElementById("createTodosForm").reset();
 }
 
 // DELETE TODOS
@@ -189,9 +234,8 @@ deleteTodos = function(id) {
     callApi('todos/' + id, 'DELETE')
         .then(function() {
             var userid = document.getElementById("userTodos").value;
-            console.log(usrId)
             alert("Delete Success!")
-            getTodos(usrId);
+            getTodos(defaultUserId);
         });
 
 }
@@ -202,10 +246,8 @@ editTodos = function(id) {
     todosId = id
     callApi('todos/' + id, 'GET')
         .then(function(response) {
-            var id = response.userId;
-            var title = response.title;
-            document.getElementById("editUserId").value = id;
-            document.getElementById("editTitle").value = title;
+            document.getElementById("editUserId").value = response.userId;
+            document.getElementById("editTitle").value = response.title;
             var editRadio = document.getElementsByName("editCompleted");
             for (let i = 0; i < editRadio.length; i++) {
                 if (JSON.parse(editRadio[i].value) === response.completed) {
@@ -216,25 +258,46 @@ editTodos = function(id) {
 }
 
 saveTodos = function() {
-    var editUserId = document.getElementById("editUserId").value;
-    var editTitle = document.getElementById("editTitle").value;
-    var editRadio = document.getElementsByName("editCompleted");
-    for (i = 0; i < editRadio.length; i++) {
-        if (editRadio[i].checked) {
-            var editCompleted = editRadio[i].value
-        }
-    }
-    var data = {
-        "userId": editUserId,
-        "title": editTitle,
-        "completed": editCompleted
-    }
 
-    callApiPost('todos/' + todosId, 'PUT', data)
-        .then(function(response) {
-            getTodos(usrId);
-            alert("Edit Success!")
-        });
+    var fEditTitle = document.forms["edittodoModal"]["editTitle"].value;
+    if (fEditTitle == "") {
+        alert("Title Must Be Filled");
+        return false;
+    } else {
+        var editUserId = document.getElementById("editUserId").value;
+        var editTitle = document.getElementById("editTitle").value;
+        var editRadio = document.getElementsByName("editCompleted");
+        for (i = 0; i < editRadio.length; i++) {
+            if (editRadio[i].checked) {
+                var editCompleted = editRadio[i].value
+            }
+        }
+
+        // CODE FILTER TODOS
+
+        callApi('todos?userId=' + editUserId, 'GET')
+            .then(function(response) {
+                var filterTitle = _.filter(response, ['title', editTitle]);
+                if (filterTitle.length === 0) {
+                    var data = {
+                        "userId": editUserId,
+                        "title": editTitle,
+                        "completed": editCompleted
+                    }
+
+                    callApi('todos/' + todosId, 'PUT', data)
+                        .then(function(response) {
+                            getTodos(defaultUserId);
+                            alert("Edit Success!")
+                        });
+                    $('#edittodoModal').modal('hide')
+                } else {
+                    alert("Title exists");
+                    return false;
+                }
+            })
+
+    }
 }
 
 // // SEARCH POST
@@ -242,7 +305,7 @@ searchPost = function() {
 
     let $el = document.getElementById("posts")
     var username = document.getElementById("find").value;
-
+    document.getElementById("form").reset();
     callApi('users?name=' + username, 'GET')
         .then(function(response) {
             console.log(response)
